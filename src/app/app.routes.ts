c964b6c1741583a62cf426a959951c97963cd693
shubhamkaviskar <134922894+shubhamkaviskar@users.noreply.github.com>
2024-09-24 01:42:29 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: ' ',component: HomeComponent},
+    {path: 'product',component: ProductComponent},
+    {path:'login',component: LoginComponent},
+    {path:'signup',component: SignupComponent}
+];
